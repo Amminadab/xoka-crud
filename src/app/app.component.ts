d66d13108aa660883d065d748ea1a2287b3855f8
Amminadab <amminadabeliasfant@gmail.com>
2023-05-09 11:45:04 +0300

    added some functionality on the company component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CompAddEditComponent } from './comp-add-edit/comp-add-edit.component';
+import { CompanyService } from './services/company.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'human-resourse';
+export class AppComponent implements OnInit {
+  title = 'human-resource';
+
+  displayedData: string[] = [
+    'id',
+    'companyName',
+    'companyTrademark',
+    'location',
+    'phoneNumber',
+  ];
+
+  dataSource = [];
+
+  constructor(
+    private _dialog: MatDialog,
+    private _compService: CompanyService
+  ) {}
+
+  ngOnInit(): void {
+    this.getCompanyList();
+  }
+
+  openAddEditCompForm() {
+    const dialogRef = this._dialog.open(CompAddEditComponent);
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getCompanyList();
+        }
+      },
+    });
+  }
+
+  getCompanyList() {
+    this._compService.getCompanyList().subscribe({
+      next: (res) => {
+        this.dataSource = res;
+        // this.getCompanyList();
+      },
+      error: console.log,
+    });
+  }
+  openEditCompForm(data: any) {
+    this._dialog.open(CompAddEditComponent, { data });
+  }
 }
